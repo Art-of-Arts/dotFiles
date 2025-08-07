9471a15d1963e8521a06c0840766947d4f9c19d0
arts <a@a.a>
2025-08-07 09:31:34 +0100

    fixed up and updated package names

--- scripts/firstTimeScripts/generalInstall.sh
+++ scripts/firstTimeScripts/generalInstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo /bin/pacman -S --needed --noconfirm tlp rangoli cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqualculate ddcutil discover flatpak dolphin fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf
+sudo /bin/pacman -S --needed --noconfirm tlp cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak dolphin fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv
 
 if [[ "$(hostnamectl hostname)" = "holo-machine" ]]; then
   sudo /bin/pacman -S fprintd
 fi
 
 if [[ -f "/bin/yay" ]]; then
-  /bin/yay -S --needed --noconfirm tlp audiorelay rose-pine-gtk-theme xwaylandbridge 
+  /bin/yay -S --needed --noconfirm tlp audiorelay rose-pine-gtk-theme xwaylandvideobridge rangoli 
 else
   echo "yay not installed, install yay and re-run the script"
 fi

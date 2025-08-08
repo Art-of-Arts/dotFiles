9d31c8239078234aa0460ec89f315561fd22be09
Art of Arts <a@a.a>
2025-08-08 02:36:26 +0100

    upgrades people upgrades!

--- scripts/firstTimeScripts/hyprlandInstaller.sh
+++ scripts/firstTimeScripts/hyprlandInstaller.sh
@@ -16,4 +16,9 @@
 
 sudo /bin/pacman -S --noconfirm --needed hyprland hyprlang hyprlock hyprshot hypridle hyprutils hyprpicker hyprsunset hyprcursor hyprgraphics hyprpolkitagent hyprland-qtutils hyprland-protocols hyprland-qt-support hyprwayland-scanner xdg-desktop-portal-hyprland waybar wofi nwg-displays gimp swaync
 
-/bin/yay -S --noconfirm --needed hyprshell tlpui
+if [[ -z "/bin/yay" ]]; then
+  if ! [[ "$(cat /etc/hostname)" = "holo-machine" ]] ; then
+    /bin/yay -S --noconfirm --needed tlpui
+  fi
+  /bin/yay -S --noconfirm --needed hyprshell
+fi

fca460719341980f29cee00dc257afe60ad91697
arts <a@a.a>
2025-08-24 02:36:25 +0100

    yazi update

--- scripts/firstTimeScripts/generalInstall.sh
+++ scripts/firstTimeScripts/generalInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo /bin/pacman -S --needed --noconfirm cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak yazi fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv lsd
+sudo /bin/pacman -S --needed --noconfirm cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak yazi fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv lsd trash-cli
 
 if [[ "$(hostnamectl hostname)" = "holo-machine" ]]; then
   sudo /bin/pacman -S tlp fprintd

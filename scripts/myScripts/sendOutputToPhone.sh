8d4552369c7dea29a082ea434b0178638288188b
arts <a@a.a>
2025-10-12 11:12:45 +0100

    no longer autolaunching because i use the flatpak version or something idk just no more autolaunch

--- scripts/myScripts/sendOutputToPhone.sh
+++ scripts/myScripts/sendOutputToPhone.sh
@@ -10,7 +10,7 @@ trap "suspendHandle trapping ctrl + z or ctrl + c" 20 2 1 3
 pactl get-default-sink > /tmp/PACTL.DEFAULT.SINK
 pactl load-module module-null-sink sink_name=Audio-to-phone > /tmp/VSINK.MODULE.ID
 pactl set-default-sink Audio-to-phone
-audiorelay $1 & > /tmp/AUDIO.RELAY.PID
+# audiorelay $1 & > /tmp/AUDIO.RELAY.PID
 
 notify-send -u low -a 'audiorelay' -t 3 "created virtual sink and opened audiorelay minimized"
 
@@ -19,7 +19,7 @@ read -n 1
 
 pactl set-default-sink $(cat /tmp/PACTL.DEFAULT.SINK)
 pactl unload-module $(cat /tmp/VSINK.MODULE.ID)
-for pid in $(ps -e aux | awk '/audiorelay/ {print $2}'); do kill -9 $pid; done
+# for pid in $(ps -e aux | awk '/audiorelay/ {print $2}'); do kill -9 $pid; done
 
 notify-send -u low -a 'audiorelay' -t 3 "closed audiorelay and virtual sink"
 rm /tmp/{PACTL.DEFAULT.SINK,VSINK.MODULE.ID}

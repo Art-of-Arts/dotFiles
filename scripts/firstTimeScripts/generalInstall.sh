99866a17b7662dff4c4c39b7b53c830a9a4854ab
arts <a@a.a>
2025-10-28 01:44:48 +0000

    first time install scripts update

--- scripts/firstTimeScripts/generalInstall.sh
+++ scripts/firstTimeScripts/generalInstall.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-sudo /bin/pacman -Sy --needed --noconfirm cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak yazi fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv lsd trash-cli imagemagick
+sudo /bin/pacman -Sy --needed --noconfirm cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak yazi fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv lsd trash-cli imagemagick wiremix
 
 if [[ "$(hostnamectl hostname)" = "holo-machine" ]]; then
   sudo /bin/pacman -Sy --needed --noconfirm tlp fprintd
   if [[ -f "/bin/yay" ]]; then
     /bin/yay -Sy --needed --noconfirm tlpui
+else;
+  if [[ -f "/bin/yay" ]]; then
+    /bin/yay -Sy --needed --noconfirm new-lg4ff-dkms-git
 fi
 
 if [[ -f "/bin/yay" ]]; then

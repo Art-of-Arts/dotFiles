87551d4df9fba05a822d6cba1a3c367da1f4777d
arts <a@a.a>
2025-12-23 03:03:11 +0000

    smol changes

--- scripts/firstTimeScripts/generalInstall.sh
+++ scripts/firstTimeScripts/generalInstall.sh
@@ -3,14 +3,14 @@
 sudo /bin/pacman -Sy --needed --noconfirm cliphist wl-clipboard cmake gcc 7zip ark bitwarden blender btop curl git wget libqalculate ddcutil discover flatpak yazi fakeroot ffmpeg fastfetch ghostty gzip gvim brightnessctl less man-db obs-studio obsidian openssh playerctl steam spotify-launcher swaync telegram-desktop tmux v4l2loopback-dkms v4l2loopback-utils vlc vlc-cli zoxide fzf direnv lsd trash-cli imagemagick wiremix tlp nftables 
 
 if [[ "$(hostnamectl hostname)" = "holo-machine" ]]; then
-  sudo /bin/pacman -Sy --needed --noconfirm fprintd
+  sudo /bin/pacman -Sy --needed --noconfirm fprintd tlp
 fi
 
 if [[ -f "/bin/yay" ]]; then
   /bin/yay -Sy --needed --noconfirm rose-pine-gtk-theme xwaylandvideobridge rangoli kando-bin gcalcli arronax dbus_filemanager tlpui mullvad-vpn-bin
 
   if [[ "$(hostnamectl hostname)" = "holo-machine" ]]; then
-    /bin/yay -Sy --needed --noconfirm 
+    /bin/yay -Sy --needed --noconfirm tlpui
   else
     /bin/yay -Sy --needed --noconfirm new-lg4ff-dkms-git
   fi

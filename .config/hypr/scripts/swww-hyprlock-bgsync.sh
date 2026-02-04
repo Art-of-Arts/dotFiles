c859eab1621f432627e6b8ea2e69f3f3a3c3e98e
arts <a@a.a>
2026-02-04 04:53:57 +0000

    works with multiple monitors now ( only uses the first one though )

--- .config/hypr/scripts/swww-hyprlock-bgsync.sh
+++ .config/hypr/scripts/swww-hyprlock-bgsync.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-BLURSTR=80
+BLURSTR=10
 
 if ! [[ -d "/tmp/hyprlock-cache/" ]]; then
   echo "cache directory doesn't exist, making one now."
   mkdir -p "/tmp/hyprlock-cache/"
 fi
 
-ffmpeg -i "$(swww query | awk 'BEGIN{FS=": "}{printf $6}')" -y -vf "gblur=sigma=$BLURSTR" -vframes 1 "/tmp/hyprlock-cache/bgframe.jpg"
+ffmpeg -i "$(swww query | awk 'BEGIN{FS=": "}NR==1{printf $6}')" -y -vf "gblur=sigma=$BLURSTR" -vframes 1 "/tmp/hyprlock-cache/bgframe.jpg"

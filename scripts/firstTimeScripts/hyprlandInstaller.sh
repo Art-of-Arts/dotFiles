7a5fc35ec438bbf75ae955212b4231e036454c9e
arts <a@a.a>
2026-03-23 02:22:17 +0000

    hyprlandInstaller updated
    mouse mode stuffs, in case i don't have a mouse on hand somehow, plus
    using the compuber widoud a maus is preddy coool :thumbs up:

--- scripts/firstTimeScripts/hyprlandInstaller.sh
+++ scripts/firstTimeScripts/hyprlandInstaller.sh
@@ -1,24 +1,10 @@
-#!/bin/bash
-
-# function __ROOTPWASK() {
-#   if $(sudo whoami); then
-#     while true; do sudo -v &>/dev/null; sleep 240; done &
-#   else
-#     echo "wrong password?"
-#     __ROOTPWASK
-#   fi
-# }
-# 
-# if ! [[ $(whoami) = "root" ]]; then
-#   __ROOTPWASK # gets the root password and re-auths every 4 mins if download takes longer than that
-# fi
- 
+#!/usr/bin/env bash
 
 sudo /bin/pacman -S --noconfirm --needed hyprland hyprlang hyprlock hyprshot hypridle hyprutils hyprpicker hyprsunset hyprcursor hyprgraphics hyprland-qtutils hyprland-protocols hyprland-qt-support hyprwayland-scanner xdg-desktop-portal-hyprland waybar wofi nwg-displays gimp swaync papirus-icon-theme polkit polkit-gnome jq
 
 if [[ -z "/bin/yay" ]]; then
-  if ! [[ "$(cat /etc/hostname)" = "holo-machine" ]] ; then
+  if ! [[ "$(cat /etc/hostname)" = "holo-machine" ]]; then
     /bin/yay -S --noconfirm --needed tlpui
   fi
-  /bin/yay -S --noconfirm --needed vigiland-git
+  /bin/yay -S --noconfirm --needed vigiland-git wl-kbptr wlrctl
 fi

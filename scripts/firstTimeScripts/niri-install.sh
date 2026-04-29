8d2a7907b9e05bc35fa213ec4ee0fd4c751d02f0
arts <a@a.a>
2026-04-29 04:27:52 +0100

    more niri + additional niri-install script
    it quite shrimple and i'm lazy

---
+++ scripts/firstTimeScripts/niri-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+pacman -S --noconfirm --needed niri xwayland-satellite swaylock
+
+if [[-z "/bin/yay" ]]; then
+  if ! [[ "$(cat /etc/hostname)" = "holo-machine" ]]; then
+    /bin/yay -S --noconfirm --needed tlpui
+  fi
+  /bin/yay -S --noconfirm --needed vigiland-git wl-kbptr
+fi

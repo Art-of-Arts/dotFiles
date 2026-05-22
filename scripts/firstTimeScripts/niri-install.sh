0e9b2582a3fc526cc312b67e6092ea34245d65cd
arts <a@a.a>
2026-05-22 12:08:09 +0100

    large lazy commit
    ghostty:  background opaque becaues i still
              haven't figured out transparency and blur
    
    kitty:    fixed up config while testing
    
    niri:     normal_keybinds split form config
              ( might switch from full niri keybinds
              to hybrid between niri and kanata )
    
    icons:    now syncing my cursors
    
    scripts:  updated installers with new packages

--- scripts/firstTimeScripts/niri-install.sh
+++ scripts/firstTimeScripts/niri-install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-sudo pacman -S --noconfirm --needed niri xdg-desktop-portal xdg-desktop-portal-gnome xwayland-satellite swaylock
+sudo pacman -S --noconfirm --needed niri xdg-desktop-portal xdg-desktop-portal-gnome xwayland-satellite swaylock waybar rofi nwg-displays swaync polkit polkit-gnome jq ydotool
 
 if [[-z "/bin/yay" ]]; then
   if ! [[ "$(cat /etc/hostname)" = "holo-machine" ]]; then
-    /bin/yay -S --noconfirm --needed tlpui
+    /bin/yay -S --noconfirm --needed
   fi
   /bin/yay -S --noconfirm --needed vigiland-git wl-kbptr
+else
+  echo "yay not installed, install yay and re-run the script to install packages from the aur"
 fi
